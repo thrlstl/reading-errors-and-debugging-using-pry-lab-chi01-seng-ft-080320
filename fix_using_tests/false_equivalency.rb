ee8fe7c40a1d803a904830925291efa7aadc01de
Matthew Steele <matthewsteeleonline@gmail.com>
2020-07-23 04:14:40 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -11,11 +11,9 @@ def prompt_user
 end
 
 def selection(num)
-  if num = 1
-    binding.pry
+  if num == 1
      "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
-  binding.pry
+  elsif num == 2
      "HAM HAM HAM IN MY TUMMY"
   end
 end
