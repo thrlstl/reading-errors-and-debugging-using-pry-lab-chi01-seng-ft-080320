729d00e0b3968a7c1e03c410b3e0c3e05c735c8d
Matthew Steele <matthewsteeleonline@gmail.com>
2020-07-23 04:43:00 +0000

    Done.

--- spec/false_equivalency_spec.rb
+++ spec/false_equivalency_spec.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 require_relative '../fix_using_tests/false_equivalency'
 
 describe "false_equivalency" do
--- spec/method_man_spec.rb
+++ spec/method_man_spec.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 require_relative '../fix_using_tests/method_man'
 
 describe "method_man" do
